0dd7cf5cad94278d21d0f376f93d3076dd6baab7
kadulkaryash71 <yashkadulkar@gmail.com>
2023-10-11 13:04:58 +0100

    Life coaching and Purpose section
    All placeholders. Might need more assets and animations. Can be more interactive.

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,15 +5,6 @@ function Content() {
     <section className="px-10 mt-16">
         <h2 className="text-2xl font-bold">Contents of the webpage</h2>
         <ul className="list-disc">
-            <li>
-                Hero section
-                <ul className="indent-4">
-                    <li>Vectors and interactive design</li>
-                    <li>Color scheme and font design</li>
-                    <li>Title of the whole system</li>
-                    <li>Transitions and animations for the title from CodePen</li>
-                </ul>
-            </li>
             <li>Why Life Coaching is important for you?</li>
             <li>How we can help</li>
             <li>
@@ -41,6 +32,15 @@ function Content() {
                 Navbar
                 <ul className="indent-4">
                     <li>Logo design</li>
+                </ul>
+            </li>
+            <li>
+                Hero section
+                <ul className="indent-4">
+                    <li>Vectors and interactive design</li>
+                    <li>Color scheme and font design</li>
+                    <li>Title of the whole system</li>
+                    <li>Transitions and animations for the title from CodePen</li>
                 </ul>
             </li>
         </ul>
--- src/pages/hello.js
+++ src/pages/hello.js
@@ -2,6 +2,10 @@ import React from 'react'
 import Navbar from '../components/Navbar';
 import Content from '../components/Content';
 import Hero from '../components/Hero';
+import Jumbotron from '../components/Jumbotron';
+import Purpose from '../components/Purpose';
+import mindVector from '../images/icons/mind-vector.svg'
+
 
 function Hello() {
     return (
@@ -11,6 +15,13 @@ function Hello() {
             </header>
             <Content />
             <Hero />
+            <Jumbotron 
+                header="What is Life Coaching?" 
+                subheader="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem, dolores?" 
+                body="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate praesentium odit, minima, explicabo cum mollitia facere hic ipsa officiis quo laboriosam deleniti? Rem aspernatur totam dignissimos debitis dicta provident ratione!" 
+                imgsrc={mindVector}
+            />
+            <Purpose />
         </article>
     )
 }
